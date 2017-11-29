dc898abe4a301a6dd4c9c0c9697c096ea0824b8c
deread <1156442891@qq.com>
2017-11-29 17:57:40 +0800

    修改订单确认和拒绝功能bug

--- controllers/PutOrderStatController.go
+++ controllers/PutOrderStatController.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	"iHome_go_1/models"
@@ -24,9 +26,11 @@ func (this *PutOrderStatController) RetData(resp interface{}) {
 }
 
 //api/v1.0/orders/:id/status-->struct
+/*
 type StructAction struct {
 	Action string `json:"action"` //accept  ,reject
 }
+*/
 
 func (this *PutOrderStatController) PutOrderStat() {
 	resp := PutOrderStatResp{Errno: models.RECODE_OK, Errmsg: models.RecodeText(models.RECODE_OK)}
@@ -40,23 +44,27 @@ func (this *PutOrderStatController) PutOrderStat() {
 	}
 
 	//通过url得到当前订单ID
-	order_id := this.Input().Get("id")
+	//order_id := this.Input().Get("id")
+	order_id := this.Ctx.Input.Param(":id")
 	int_order_id, err := strconv.Atoi(order_id)
+	fmt.Println("int_order_id-->", int_order_id)
 	//解析json数据，得到action
-	action := StructAction{}
-	err = this.ParseForm(&action)
+
+	var req map[string]interface{}
+	json.Unmarshal(this.Ctx.Input.RequestBody, &req)
 	if err != nil {
-		resp.Errno = models.RECODE_DATAERR
+		resp.Errno = models.RECODE_REQERR
 		resp.Errmsg = models.RecodeText(resp.Errno)
 		return
 	}
+	action := req["action"]
+	fmt.Println("action-->", action)
 	//校验action合法性
-	if action.Action != "accept" && action.Action != "reject" {
+	if action != "accept" && action != "reject" {
 		resp.Errno = models.RECODE_PARAMERR
 		resp.Errmsg = models.RecodeText(resp.Errno)
 		return
 	}
-
 	//查找订单表order_house，得到订单并确定订单状态是WAIT_ACCEPT
 	OrderHouse := models.OrderHouse{}
 	o := orm.NewOrm()
@@ -66,40 +74,40 @@ func (this *PutOrderStatController) PutOrderStat() {
 		resp.Errmsg = models.RecodeText(resp.Errno)
 		return
 	}
-	//校验订单的user_id是否是当前用户的user_id
-	if v != OrderHouse.User.Id {
-		resp.Errno = models.RECODE_USERERR
-		resp.Errmsg = models.RecodeText(resp.Errno)
-		return
-	}
+	/*
+		//校验订单的user_id是否是当前用户的user_id
+		if v.(int) != OrderHouse.User.Id {
+			resp.Errno = models.RECODE_USERERR
+			resp.Errmsg = models.RecodeText(resp.Errno)
+			return
+		}
+	*/
 	//校验houseOrder状态
 	Order_stat := OrderHouse.Status
-	if Order_stat != "WAIT_ACCEPT" {
+	if Order_stat != models.ORDER_STATUS_WAIT_ACCEPT {
 		resp.Errno = models.RECODE_PARAMERR
 		resp.Errmsg = models.RecodeText(resp.Errno)
 		return
 	}
 	//if action=accept-->更换订单status状态WAIT_COMMIT等待用户评价
-	if action.Action == "accept" {
+	if action == "accept" {
 		OrderHouse.Status = models.ORDER_STATUS_WAIT_COMMENT
 		//else  action=reject-->更换订单状态status状态REJECT
-	} else if action.Action == "reject" {
+	} else if action == "reject" {
 		OrderHouse.Status = models.ORDER_STATUS_REJECTED
 		//从url中获取reason参数字段
-		Rjct_reason := this.Input().Get("reason")
-
+		Rjct_reason := req["reason"]
 		//将reason字段添加到order的评价Comment字段中
-		OrderHouse.Comment = Rjct_reason
+		OrderHouse.Comment = Rjct_reason.(string)
+		fmt.Println("OrderHouse_comment-->", OrderHouse)
 	}
 
 	//更新mysql数据库中的订单
-	if o.Read(&OrderHouse) == nil {
-		_, err = o.Update(&OrderHouse)
-		if err != nil {
-			resp.Errno = models.RECODE_DBERR
-			resp.Errmsg = models.RecodeText(resp.Errno)
-			return
-		}
+	_, err = o.Update(&OrderHouse)
+	if err != nil {
+		resp.Errno = models.RECODE_DBERR
+		resp.Errmsg = models.RecodeText(resp.Errno)
+		return
 	}
 	//返回前端
 	return
